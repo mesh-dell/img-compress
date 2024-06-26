ddad71bce7438022771bcb57872a150d5392e408
mesh-dell <mwaimonicas@gmail.com>
2024-06-26 22:34:22 +0300

    feat(image-compression): Compress supported image formats using sharp

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { program } = require("commander");
 const { opendir } = require("node:fs/promises");
+const sharp = require("sharp");
 const path = require("node:path");
 
 program
@@ -18,7 +19,7 @@ program
 
     // try to open all files in the path
     try {
-      getImages(inputPath);
+      getImages(inputPath, options);
     } catch (err) {
       console.error(err);
     }
@@ -36,17 +37,55 @@ const imageExtensions = [
   ".tiff",
 ];
 
-async function getImages(dirPath) {
+async function getImages(dirPath, options) {
   try {
     const dir = await opendir(dirPath);
     for await (const dirent of dir) {
       const extension = path.extname(dirent.name).toLowerCase();
 
       if (imageExtensions.includes(extension)) {
-        console.log(`Image found: ${dirent.name}`);
+        const inputFilePath = path.join(dirPath, dirent.name);
+        const outputPath = options.output || dirPath;
+        const outputFilePath = path.join(
+          outputPath,
+          `${path.basename(dirent.name, extension)}_compressed${extension}`
+        );
+        await compressImage(inputFilePath, outputFilePath, extension);
       }
     }
   } catch (err) {
     console.error(err);
   }
+}
+
+async function compressImage(inputFilePath, outputFilePath, extension) {
+  const image = sharp(inputFilePath);
+  switch (extension) {
+    case ".jpeg":
+    case ".jpg":
+      await image.jpeg({ quality: 80, mozjpeg: true }).toFile(outputFilePath);
+      break;
+    case ".png":
+      await image
+        .png({ compressionLevel: 9, adaptiveFiltering: true, palette: true })
+        .toFile(outputFilePath);
+      break;
+    case ".webp":
+      await image.webp({ quality: 80 }).toFile(outputFilePath);
+      break;
+    case ".gif":
+      await image.gif({ colors: 128 }).toFile(outputFilePath);
+      break;
+    case ".avif":
+      await image.avif({ quality: 50, effort: 9 }).toFile(outputFilePath);
+      break;
+    case ".tiff":
+      await image
+        .tiff({ compression: "lzw", predictor: "horizontal" })
+        .toFile(outputFilePath);
+      break;
+    default:
+      throw new Error(`Unsupported image format: ${extension}`);
+  }
+  console.log(`Compressed: ${outputFilePath}`);
 }
